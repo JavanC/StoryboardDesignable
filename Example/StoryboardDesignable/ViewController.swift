5ec71585acac5b99b13c4b2c3441f71563e2cafd
Javan.Chen <fu6gjp4@gmail.com>
2017-06-11 23:10:49 +0800

    Add Example Project

---
+++ Example/StoryboardDesignable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoryboardDesignable
+//
+//  Created by Javan on 2017/6/11.
+//  Copyright © 2017年 Javan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
